184d076f740b83c9f96014f5002f42888ae87d29
isao_nakamura <hoge>
2015-08-10 17:56:13 +0900

    タブとかスペースとかコメントなどを修正

--- Lib/drivers/RPiGpioDrv/RPiGpioDrv.cpp
+++ Lib/drivers/RPiGpioDrv/RPiGpioDrv.cpp
@@ -8,8 +8,8 @@
 #define GPIO_PIN_MAX		(31)
 #define GPIO_PIN_MIN		( 0)
 
-//  GPIO 関連レジスタ (volatile＝必ず実メモリにアクセスさせる)
-static volatile unsigned int *g_pGpio;
+// GPIOレジスタ (volatile:実メモリに必ずアクセス)
+static volatile unsigned int *g_pGpio = NULL;
 
 RPiGpioDrv::RPiGpioDrv()
 {
@@ -19,11 +19,11 @@ RPiGpioDrv::~RPiGpioDrv()
 {
 }
 
-//  GPIO 初期化
+// 初期化
 int RPiGpioDrv::init(const int& RPiVer/*=2*/)
 {
-    //  既に初期化済なら何もしない
-    if(g_pGpio){
+	if(g_pGpio){
+		// 既に初期化済
 		return 0;
 	}
 	
@@ -38,85 +38,98 @@ int RPiGpioDrv::init(const int& RPiVer/*=2*/)
 		return -1;
 	}
 
-    //  ここから GPIO 初期化
-    int fd;
-    void *gpio_map;
-    //  /dev/memを開く（要sudo）
-    fd = open("/dev/mem", O_RDWR | O_SYNC);
-    if(fd == -1) {
-        printf("@RPiGpioDrv::init() cannot open /dev/mem\n");
-        return -1;
-    }
+	//  GPIO初期化
+	int fd;
+	void *gpio_map;
+	//  /dev/memを開く（要sudo）
+	fd = open("/dev/mem", O_RDWR | O_SYNC);
+	if(fd == -1) {
+		printf("@RPiGpioDrv::init() cannot open /dev/mem\n");
+		return -1;
+	}
 
     //  mmap で GPIO（物理メモリ）を gpio_map（仮想メモリ）に紐づける
-    gpio_map = mmap(NULL, BLOCK_SIZE,
+	gpio_map = mmap(NULL, BLOCK_SIZE,
                     PROT_READ | PROT_WRITE, MAP_SHARED,
                     fd, register_base );
-    if((int) gpio_map == -1) {
-        printf("@RPiGpioDrv::init() cannot mmap /dev/mem\n");
-        return -1;
-    }
-    //  mmap()後はfd をクローズ
-    close(fd);
+	if((int) gpio_map == -1){
+		printf("@RPiGpioDrv::init() cannot mmap /dev/mem\n");
+		return -1;
+	}
+    //  mmap()後はfdをクローズ
+	close(fd);
 
-    g_pGpio = (unsigned int *) gpio_map;
-    
-    // 正常終了
-    return 0;
+	g_pGpio = (unsigned int *) gpio_map;
+
+	// 正常終了
+	return 0;
 }
 
-//  ピンモードの設定
-//      pin : (P1) 2,3,4,7,8,9,10,11,14,15,17,18,22,23,24,25,27
-//            (P5) 28,29,30,31
-//      mode: GPIO_INPUT, _OUTPUT, _ALT0, _ALT1, _ALT2, _ALT3, _ALT4, _ALT5
+// ピンモードの設定
+//		pin :	2,3,4,7,8,9,10,11,14,15,17,18,22,23,24,25,27,
+//				28,29,30,31
+//		mode:	GPIO_INPUT, GPIO_OUTPUT,
+//				GPIO_ALT0, GPIO_ALT1, GPIO_ALT2, GPIO_ALT3, GPIO_ALT4, GPIO_ALT5
 void int RPiGpioDrv::setPinMode(const int& pin, const int& mode)
 {
-    //  check range of pin-number.
-    if(pin < GPIO_PIN_MIN || pin > GPIO_PIN_MAX) {
-        printf("@RPiGpioDrv::setPinMode() pin number out of range\n");
-        return -1;
-    }
-    //  レジスタ番号（index）と３ビットマスクを生成
-    int index = pin / 10;
-    unsigned int mask = ~(0x7 << ((pin % 10) * 3));
-    //  GPFSEL0/1 の該当する FSEL (3bit) のみを書き換え
-    g_pGpio[index] = (g_pGpio[index] & mask) | ((mode & 0x7) << ((pin % 10) * 3));
+	if(!g_pGpio){
+		return -1;
+	}
+
+	//  check range of pin-number.
+	if(pin < GPIO_PIN_MIN || pin > GPIO_PIN_MAX) {
+		printf("@RPiGpioDrv::setPinMode() pin number out of range\n");
+		return -1;
+	}
+	//  レジスタ番号(index)と3bitマスクを生成
+	int index = pin / 10;
+	unsigned int mask = ~(0x7 << ((pin % 10) * 3));
+	//  GPFSEL0/1の該当するFSEL(3bit)のみを書き換え
+	g_pGpio[index] = (g_pGpio[index] & mask) | ((mode & 0x7) << ((pin % 10) * 3));
 }
 
 //  ピンの出力を1(HighLevel:3.3V)/0(LowLevel:0.0V)に設定
 int RPiGpioDrv::setOutLevel(const int& pin, const int& level)
 {
-    //  check range of pin-number.
-    if(pin < GPIO_PIN_MIN || pin > GPIO_PIN_MAX) {
-        printf("@RPiGpioDrv::setOutLevel() pin number out of range\n");
-        return -1;
-    }
-    
-    if(level==0){
-	    //  pin-level to LowLevel(0.0V).
-	    g_pGpio[10] = 0x1 << pin;
-    }else{
-	    //  pin-level to HighLevel(3.3V).
-    	g_pGpio[7] = 0x1 << pin;
+	if(!g_pGpio){
+		return -1;
 	}
 	
-    // success.
-    return 0;
+	//  check range of pin-number.
+	if(pin < GPIO_PIN_MIN || pin > GPIO_PIN_MAX) {
+		printf("@RPiGpioDrv::setOutLevel() pin number out of range\n");
+		return -1;
+	}
+
+	if(level==0){
+		//  pin-level to LowLevel(0.0V).
+		g_pGpio[10] = 0x1 << pin;
+    }else{
+		//  pin-level to HighLevel(3.3V).
+    	g_pGpio[7] = 0x1 << pin;
+	}
+
+	// success.
+	return 0;
 }
 
 // ピンの状態を1(HighLevel:3.3V)/0(LowLevel:0.0V)として取得
 int RPiGpioDrv::getLevel(const int& pin, int& level)
 {
+	if(!g_pGpio){
+		return -1;
+	}
+
 	// clear output.
 	level = 0;
 
     //  check range of pin-number.
-    if(pin < GPIO_PIN_MIN || pin > GPIO_PIN_MAX) {
-       printf("@RPiGpioDrv::setOutLevel() pin number out of range\n");
-        return -1;
-    }
+	if(pin < GPIO_PIN_MIN || pin > GPIO_PIN_MAX){
+		printf("@RPiGpioDrv::setOutLevel() pin number out of range\n");
+		return -1;
+	}
 
-    level = ( (g_pGpio[13] & (0x1 << pin)) != 0 );
+	level = ( (g_pGpio[13] & (0x1 << pin)) != 0 );
 
     // success.
     return 0;
--- Lib/drivers/RPiGpioDrv/RPiGpioDrv.h
+++ Lib/drivers/RPiGpioDrv/RPiGpioDrv.h
@@ -7,15 +7,15 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
-//  ピン機能（BCM2835）
-#define GPIO_INPUT    0x0       //  入力
-#define GPIO_OUTPUT   0x1       //  出力
-#define GPIO_ALT0     0x4
-#define GPIO_ALT1     0x5
-#define GPIO_ALT2     0x6
-#define GPIO_ALT3     0x7
-#define GPIO_ALT4     0x3
-#define GPIO_ALT5     0x2
+//  ピン機能
+#define GPIO_INPUT	(0x0)	//  入力
+#define GPIO_OUTPUT	(0x1)	//  出力
+#define GPIO_ALT0	(0x4)
+#define GPIO_ALT1	(0x5)
+#define GPIO_ALT2	(0x6)
+#define GPIO_ALT3	(0x7)
+#define GPIO_ALT4	(0x3)
+#define GPIO_ALT5	(0x2)
 
 class RPiGpioDrv
 {
@@ -28,9 +28,10 @@ public:
 	static int init(const int& RPiVer=2);
 	
 	// ピンモードの設定
-	//      pin : (P1) 2,3,4,7,8,9,10,11,14,15,17,18,22,23,24,25,27
-	//            (P5) 28,29,30,31
-	//      mode: GPIO_INPUT, _OUTPUT, _ALT0, _ALT1, _ALT2, _ALT3, _ALT4, _ALT5
+	//		pin :	2,3,4,7,8,9,10,11,14,15,17,18,22,23,24,25,27,
+	//				28,29,30,31
+	//		mode:	GPIO_INPUT, GPIO_OUTPUT,
+	//				GPIO_ALT0, GPIO_ALT1, GPIO_ALT2, GPIO_ALT3, GPIO_ALT4, GPIO_ALT5
 	static int setPinMode(const int& pin, const int& mode);
 	
 	// ピンの出力を1(HighLevel:3.3V)/0(LowLevel:0.0V)に設定
