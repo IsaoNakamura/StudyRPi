e4b5bbe469147f1de8660fdb4807f86e87a068b1
IsaoNakamura <gariox7@gmail.com>
2015-09-24 02:39:50 +0900

    add Comment to homingFace.sh.

--- App/homingFace/homingFace.sh
+++ App/homingFace/homingFace.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# /etc/rc.local に
+# 	sudo -u pi /home/pi/GitHub/StudyRPi/App/homingFace/homingFace.sh
+# というような本スクリプトを実行する命令を exit 0 の前に記載すれば
+# 起動時に実行できるようになる。
+
 sudo /home/pi/GitHub/StudyRPi/App/homingFace/homingFace
 
