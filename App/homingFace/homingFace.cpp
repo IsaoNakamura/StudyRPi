bb8f9adbcb5ad9f3fe5e312fe9e9505db6761def
IsaoNakamura <gariox7@gmail.com>
2015-09-23 21:20:17 +0900

    working.

--- App/homingFace/homingFace.cpp
+++ App/homingFace/homingFace.cpp
@@ -107,36 +107,9 @@ int main(int argc, char* argv[])
 		pwmSetMode(PWM_MODE_MS);
 		pwmSetClock(400);
 		pwmSetRange(1024);
-		
-		/*
-		pinMode(GPIO_EXIT, OUTPUT);
-		digitalWrite(GPIO_EXIT, 0);
-		if( digitalRead(GPIO_EXIT) == HIGH){
-			printf("GPIO_EXIT(o) is HIGHT\n");
-		}else{
-			printf("GPIO_EXIT(o) is LOW\n");
-		}
-		pinMode(GPIO_HALT, OUTPUT);
-		digitalWrite(GPIO_HALT, 1);
-		if( digitalRead(GPIO_HALT) == HIGH){
-			printf("GPIO_HALT(o) is HIGHT\n");
-		}else{
-			printf("GPIO_HALT(o) is LOW\n");
-		}
-		*/
 				
 		pinMode(GPIO_EXIT, INPUT);
-		if( digitalRead(GPIO_EXIT) == HIGH){
-			printf("GPIO_EXIT(i) is HIGHT\n");
-		}else{
-			printf("GPIO_EXIT(i) is LOW\n");
-		}
 		pinMode(GPIO_HALT, INPUT);
-		if( digitalRead(GPIO_HALT) == HIGH){
-			printf("GPIO_HALT(i) is HIGHT\n");
-		}else{
-			printf("GPIO_HALT(i) is LOW\n");
-		}
 
 		// servoMotor GWS park hpx min25 mid74 max123
 		const int servo_mid = 76;
@@ -217,6 +190,17 @@ int main(int argc, char* argv[])
 
 		int over_cnt = 0;
 		int nonface_cnt = 0;
+		
+		if( digitalRead(GPIO_EXIT) == HIGH){
+			printf("GPIO_EXIT(i) is HIGHT\n");
+		}else{
+			printf("GPIO_EXIT(i) is LOW\n");
+		}
+		if( digitalRead(GPIO_HALT) == HIGH){
+			printf("GPIO_HALT(i) is HIGHT\n");
+		}else{
+			printf("GPIO_HALT(i) is LOW\n");
+		}
 
 		// メインループ
 		while(1){
