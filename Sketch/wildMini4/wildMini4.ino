e7219527f6bb1a7ff1573be0feb029b148b137cd
IsaoNakamura <gariox7@gmail.com>
2015-11-17 17:59:47 +0900

    update Sketch/wildMini4

--- Sketch/wildMini4/wildMini4.ino
+++ Sketch/wildMini4/wildMini4.ino
@@ -1,58 +1,81 @@
 #include <Servo.h>
 
+#define PIN_SERVO   3
+#define PIN_ACCEL   4
+#define PIN_FORWARD 8
+#define PIN_BACK    9
+
+#define SERIAL_BAUDRATE          9600
+#define SERIAL_DELIMITER         0x7E
+#define SERIAL_FRAMETYPE_MOTOR   0x0
+#define SERIAL_NUM               3
+#define SERIAL_IDX_TYPE          0
+#define SERIAL_IDX_YAW           1
+#define SERIAL_IDX_ACCEL         2
+
 Servo myservo;
 int preServoYaw = 90;
 int preMotorAccel = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-   // start serial port at 9600 bps:
-  Serial.begin(9600);
+  Serial.begin(SERIAL_BAUDRATE);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
 
-  myservo.attach( 3 );
+  myservo.attach( PIN_SERVO );
   myservo.write(preServoYaw);
 
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
+  pinMode(PIN_FORWARD, OUTPUT);
+  pinMode(PIN_BACK, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // if we get a valid byte, read analog ins:
-  if (Serial.available() > 3) {
+  if (Serial.available() > SERIAL_NUM) {
     // get incoming byte:
     int inByte = Serial.read();
 
-    if(inByte == 0x7E ){ // 開始デリミタ取得
-      //digitalWrite(9, HIGH);
-      // データ格納数取得
-      int readBuf[3] = {0x0};
-      for(int i=0; i<3; i++){
+    if(inByte == SERIAL_DELIMITER ){ // 開始デリミタ取得
+      int readBuf[SERIAL_NUM] = {0x0};
+      for(int i=0; i<SERIAL_NUM; i++){
         readBuf[i] = Serial.read();
       }
-      if( preServoYaw != readBuf[1] ){
-        myservo.write(readBuf[1]);
-        preServoYaw = readBuf[1];
-      }
-      if( preMotorAccel != readBuf[2] ){
-        if(readBuf[2] == 90 ){
-          // STOP
-          analogWrite(8, 0);
-          analogWrite(9, 0);
-        }else if( (readBuf[2] > 90) && (readBuf[2] <= 180) ){
-          // FORWARD
-          analogWrite(8, readBuf[2]);
-          analogWrite(9, 0);
-        }else if( (readBuf[2] < 90) && (readBuf[2] >= 0 ) ){
-          // BACK
-          analogWrite(8, 0);
-          analogWrite(9, readBuf[2]);
+      
+      if( readBuf[SERIAL_IDX_YAW] == SERIAL_FRAMETYPE_MOTOR ){
+        // FrameType is Motor-Parameter
+
+        // Ctrl Servo-Motor
+        if( preServoYaw != readBuf[SERIAL_IDX_YAW] ){
+          myservo.write(readBuf[SERIAL_IDX_YAW]);
+          preServoYaw = readBuf[SERIAL_IDX_YAW];
         }
-        preMotorAccel = readBuf[2];
+  
+        // Ctrl DC-Motor
+        if( preMotorAccel != readBuf[SERIAL_IDX_ACCEL] ){
+          if(readBuf[SERIAL_IDX_ACCEL] == 90 ){
+            // STOP
+            digitalWrite(PIN_FORWARD, LOW);
+            digitalWrite(PIN_BACK, LOW);
+            analogWrite(PIN_ACCEL, 0);
+  
+          }else if( (readBuf[SERIAL_IDX_ACCEL] > 90) && (readBuf[SERIAL_IDX_ACCEL] <= 180) ){
+            // FORWARD
+            digitalWrite(PIN_FORWARD, HIGH);
+            digitalWrite(PIN_BACK, LOW);
+            int writeValue = 255 * (readBuf[SERIAL_IDX_ACCEL] - 90) / 90;
+            analogWrite(PIN_ACCEL, writeValue);
+  
+          }else if( (readBuf[SERIAL_IDX_ACCEL] < 90) && (readBuf[SERIAL_IDX_ACCEL] >= 0 ) ){
+            // BACK
+            digitalWrite(PIN_FORWARD, LOW);
+            digitalWrite(PIN_BACK, HIGH);
+            int writeValue = 255 - (255 * (90 - readBuf[SERIAL_IDX_ACCEL]) / 90);
+            analogWrite(PIN_ACCEL, writeValue);
+  
+          }
+          preMotorAccel = readBuf[SERIAL_IDX_ACCEL];
+        }
       }
     }
     // delay(10);
