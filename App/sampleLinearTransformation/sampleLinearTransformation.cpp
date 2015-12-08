06bbb7d31c3a9ded8f75b8efa14362d5efebc523
IsaoNakamura <gariox7@gmail.com>
2015-12-08 23:59:09 +0900

    working.

--- App/sampleLinearTransformation/sampleLinearTransformation.cpp
+++ App/sampleLinearTransformation/sampleLinearTransformation.cpp
@@ -23,6 +23,13 @@ int main(int argc, char* argv[])
 	int iRet = 0;
 	try
 	{
+		printf("SRC_MIN=%d,SRC_MID=%d,SRC_MAX=%d\n",SRC_MIN,SRC_MID,SRC_MAX);
+		printf("DST_MIN=%d,DST_MID=%d,DST_MAX=%d\n",DST_MIN,DST_MID,DST_MAX);
+		printf("LOOP_STRIDE=%d\n",LOOP_STRIDE);
+		
+		bool isOutMid = false;
+		bool isOutMax = false;
+		
 		for( int src_val=SRC_MIN; src_val<SRC_MAX; src_val+=LOOP_STRIDE){
 			int dst_val = 0;
 			if(! LinearTransformation::convertValueMinMidMax(	dst_val,
@@ -50,6 +57,66 @@ int main(int argc, char* argv[])
 				throw 0;					
 			}
 			printf("src_val=%d, dst_val=%d, dst_val_rvs=%d\n", src_val, dst_val, dst_val_rvs);
+			
+			if( ((src_val+LOOP_STRIDE)>SRC_MID) && !isOutMid ){
+				int dst_val = 0;
+				if(! LinearTransformation::convertValueMinMidMax(	dst_val,
+																	SRC_MID,
+																	SRC_MIN,
+																	SRC_MID,
+																	SRC_MAX,
+																	DST_MIN,
+																	DST_MID,
+																	DST_MAX,
+																	false		) ){
+					throw 0;					
+				}
+	
+				int dst_val_rvs = 0;
+				if(! LinearTransformation::convertValueMinMidMax(	dst_val_rvs,
+																	SRC_MID,
+																	SRC_MIN,
+																	SRC_MID,
+																	SRC_MAX,
+																	DST_MIN,
+																	DST_MID,
+																	DST_MAX,
+																	true		) ){
+					throw 0;					
+				}
+				printf("src_val=%d, dst_val=%d, dst_val_rvs=%d\n", SRC_MID, dst_val, dst_val_rvs);
+				isOutMid = true;
+			}
+			
+			if( ((src_val+LOOP_STRIDE)>SRC_MAX) && !isOutMax ){
+				int dst_val = 0;
+				if(! LinearTransformation::convertValueMinMidMax(	dst_val,
+																	SRC_MAX,
+																	SRC_MIN,
+																	SRC_MID,
+																	SRC_MAX,
+																	DST_MIN,
+																	DST_MID,
+																	DST_MAX,
+																	false		) ){
+					throw 0;					
+				}
+	
+				int dst_val_rvs = 0;
+				if(! LinearTransformation::convertValueMinMidMax(	dst_val_rvs,
+																	SRC_MAX,
+																	SRC_MIN,
+																	SRC_MID,
+																	SRC_MAX,
+																	DST_MIN,
+																	DST_MID,
+																	DST_MAX,
+																	true		) ){
+					throw 0;					
+				}
+				printf("src_val=%d, dst_val=%d, dst_val_rvs=%d\n", SRC_MAX, dst_val, dst_val_rvs);
+				isOutMid = true;
+			}
 		}
 	}
 	catch(...)
