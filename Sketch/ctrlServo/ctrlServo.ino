523a300e7a4be4a6a04ced87ecc0c422c4395bb2
IsaoNakamura <gariox7@gmail.com>
2016-06-22 07:07:21 +0900

    change ctrServo-value.

--- Sketch/ctrlServo/ctrlServo.ino
+++ Sketch/ctrlServo/ctrlServo.ino
@@ -7,18 +7,21 @@ int pos = 0;
 void setup() {
   // put your setup code here, to run once:
   //pinMode(3, OUTPUT);
-  myservo.attach( 3 );
+  myservo.attach( 10 );
+  myservo.write(65);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //analogWrite( 3, 20 );
-  for( pos=50; pos<130; pos++ ){
+  for( pos=65; pos<130; pos++ ){
     myservo.write(pos);
     delay(15);
   }
-  for( pos=130; pos>50; pos-- ){
+  for( pos=130; pos>65; pos-- ){
     myservo.write(pos);
     delay(15);
   }
+  myservo.write(65);
+  delay(2000);
 }
