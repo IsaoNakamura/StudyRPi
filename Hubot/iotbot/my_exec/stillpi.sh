9df5bbab82ff3570b97eb9d4ea3d8ae2c3752af2
IsaoNakamura <gariox7@gmail.com>
2017-01-28 23:16:23 +0900

    update stillpi.sh

--- Hubot/iotbot/my_exec/stillpi.sh
+++ Hubot/iotbot/my_exec/stillpi.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 DATE=$(date +"%Y-%m-%d_%H%M")
-raspistill $@ -o /home/pi/picam/$DATE.jpg
+if [$# > 0]
+then
+    raspistill $@ -o /home/pi/picam/$DATE.jpg
+else
+    raspistill -o /home/pi/picam/$DATE.jpg
