1145d19dd88bb7113c89c7975ccb322472df8223
IsaoNakamura <gariox7@gmail.com>
2017-01-28 23:41:46 +0900

    update stillpi.sh. add echo.

--- Hubot/iotbot/my_exec/stillpi.sh
+++ Hubot/iotbot/my_exec/stillpi.sh
@@ -3,6 +3,7 @@
 DATE=$(date +"%Y-%m-%d_%H%M")
 if [$# > 0]
 then
+    echo raspistill $@ -o /home/pi/picam/$DATE.jpg
     raspistill $@ -o /home/pi/picam/$DATE.jpg
 else
     raspistill -o /home/pi/picam/$DATE.jpg
