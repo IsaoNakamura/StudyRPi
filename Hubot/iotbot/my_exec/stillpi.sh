72884b762b3845ff4314f46a538f5922c044eb41
IsaoNakamura <gariox7@gmail.com>
2017-01-29 23:35:04 +0900

    update

--- Hubot/iotbot/my_exec/stillpi.sh
+++ Hubot/iotbot/my_exec/stillpi.sh
@@ -8,7 +8,10 @@ if [ $# -eq 0 ]; then
     # echo "no arg"
     echo "raspistill -o /home/pi/picam/$DATE.jpg"
     raspistill -o /home/pi/picam/$DATE.jpg
+elif [ $# -eq 1 ]; then
+    echo "raspistill -o $1"
+    raspistill -o $1
 else
-    echo "raspistill $@ -o /home/pi/picam/$DATE.jpg"
+    echo "raspistill ${2} -o $1"
     raspistill ${2} -o $1
 fi
