3a2ca1955b94e888416c3f8d5ca84683d87a5004
IsaoNakamura <gariox7@gmail.com>
2017-03-17 16:05:23 +0900

    update mini4SpeedChecker.

--- App/mini4SpeedChecker/mini4SpeedChecker.cpp
+++ App/mini4SpeedChecker/mini4SpeedChecker.cpp
@@ -12,9 +12,21 @@
 #include <fcntl.h>
 #include <sys/ioctl.h>
 
+// for gettimeofday
+#include <sys/time.h>
+
 #include "../../Lib/drivers/I2cAdcDrv/CI2cAdcDrv.h"
 
 #define I2C_PORT	("/dev/i2c-1")
+
+#define USE_CH 0
+
+#define PULSE_INTERVAL_DIST (0.55417693464)	//[m]
+
+#define LOOP_MAX 1000
+
+int calcSpeedPerOn( CI2cAdcDrv* pAdc );
+int calcSpeedPerSec( CI2cAdcDrv* pAdc );
 
 int main(int argc, char* argv[])
 {
@@ -22,6 +34,11 @@ int main(int argc, char* argv[])
 
 	int fd = -1;
 	CI2cAdcDrv* pAdc = NULL;
+
+	int calcSpeedMode = 0;
+	if( argc > 1 ){
+		calcSpeedMode = 1;
+	}
 
 	try
 	{
@@ -44,24 +61,18 @@ int main(int argc, char* argv[])
 			throw 0;
 		}
 		
-		int loop_cnt = 0;
-		while(loop_cnt<1000){
-			// 全チャンネルの値を取得
-			for(unsigned int i=0; i<CH_NUM; i++){
-				unsigned short		ret_value=0;
-				int					ret_state=0;
-				if( pAdc->getChannelValue(ret_value, ret_state, i)==0 ){
-					// 取得成功
-					printf("ch[%02d]:val=%05d,stat=%02d ",i,ret_value, ret_state);
-				}else{
-					// 取得失敗
-					printf("ch[%02d]:failed      ",i);
-				}
+		if(calcSpeedMode==0){
+			if( calcSpeedPerSec(pAdc) != 0){
+				printf("failed to calcSpeedPerSec()¥n");
+				throw 0;
 			}
-			printf("\n");
-			loop_cnt++;
-
-			sleep(0);
+		}else if(calcSpeedMode==1){
+			if( calcSpeedPerOn(pAdc) != 0){
+				printf("failed to calcSpeedPerOn()¥n");
+				throw 0;
+			}
+		}else{
+			printf("error!! calcSpeedMode(%d) is out of range. ¥n",calcSpeedMode);
 		}
 		
 		if(pAdc){
@@ -72,6 +83,9 @@ int main(int argc, char* argv[])
 		if(fd >= 0){
 			close(fd);
 		}
+
+		// ここまでくれば正常
+		iRet = 0;
 	}
 	catch(...)
 	{
@@ -91,29 +105,154 @@ int main(int argc, char* argv[])
 	return iRet;
 }
 
+int calcSpeedPerSec( CI2cAdcDrv* pAdc )
+{
+	int iRet = -1;
+	try
+	{
+		if(!pAdc){
+			printf("error! pAdc is NULL\n");
+			throw 0;
+		}
+
+		struct timeval stNow;	// 現時刻取得用
+		struct timeval stLen;	// 任意時間
+		struct timeval stEnd;	// 現時刻から任意時間経過した時刻
+		timerclear(&stNow);
+		timerclear(&stLen);
+		timerclear(&stEnd);
+
+		int loop_cnt = 0;
+
+		gettimeofday(&stNow, NULL);
+		stLen.tv_sec = 1;
+		stLen.tv_usec = 0;
+		timeradd(&stNow, &stLen, &stEnd);
+
+		int pulse_cnt = 0;
+
+		while(loop_cnt<LOOP_MAX){
+			// チャンネル0の値を取得
+			unsigned short		ret_value=0;
+			int					ret_state=0;
+			if( pAdc->getChannelValue(ret_value, ret_state, USE_CH)==0 ){
+				// 取得成功
+				if( ret_state > 0 ){
+					//printf("ch[%02d]:val=%05d,stat=%02d \n",USE_CH,ret_value, ret_state);
+					pulse_cnt++;
+				}
+			}else{
+				// 取得失敗
+				printf("ch[%02d]:failed      \n",0);
+			}
+
+			// 現在時刻を取得
+			gettimeofday(&stNow, NULL);
+			if( timercmp(&stNow, &stEnd, >) ){
+				// 任意時間経過
+
+				// スピードを計算
+				double speed_m_s = PULSE_INTERVAL_DIST * pulse_cnt ; //[m/s]
+				double speed_km_h = speed_m_s * 3600.0 / 1000.0;
+				printf("Speed:%03.02f[km/s]:%05.02f[m/s]", speed_km_h, speed_m_s);
+
+				// 経過時刻を更新
+				timerclear(&stEnd);
+            	gettimeofday(&stNow, NULL);
+            	timeradd(&stNow, &stLen, &stEnd);
+
+				// パルスをリセット
+				pulse_cnt = 0;
+			}
+
+
+			loop_cnt++;
+
+			sleep(0);
+		}
+	}
+	catch(...)
+	{
+		printf("calcSpeedPerOn() catch!! \n");
+		iRet = -1;
+	}
+	return iRet;
+}
+
+int calcSpeedPerOn( CI2cAdcDrv* pAdc )
+{
+	int iRet = -1;
+	try
+	{
+		if(!pAdc){
+			printf("error! pAdc is NULL\n");
+			throw 0;
+		}
+
+		struct timeval stTrue;	// パルスONを検出時の時刻取得用
+		struct timeval stLast;	// 前回パルスON時の時刻
+		timerclear(&stTrue);
+		timerclear(&stLast);
+
+		int loop_cnt = 0;
+		bool isFirstOn = true;
+		while(loop_cnt<LOOP_MAX){
+			// チャンネル0の値を取得
+			unsigned short		ret_value=0;
+			int					ret_state=0;
+			if( pAdc->getChannelValue(ret_value, ret_state, USE_CH)==0 ){
+				// 取得成功
+				if( ret_state > 0 ){
+					printf("ch[%02d]:val=%05d,stat=%02d \n",USE_CH,ret_value, ret_state);
+
+					// 現在時刻を取得
+					gettimeofday(&stTrue, NULL);
+					if(isFirstOn){
+						// 初回
+						isFirstOn = false;
+					}else{
+						// 初回以降
+
+						// スピードを計算
+						time_t diffsec = difftime(stTrue.tv_sec, stLast.tv_sec);
+						suseconds_t diffsub = stTrue.tv_usec - stLast.tv_usec;
+						double realsec = diffsec+diffsub*1e-6; //[s]
+						double speed_m_s = PULSE_INTERVAL_DIST / realsec ; //[m/s]
+						double speed_km_h = speed_m_s * 3600.0 / 1000.0;
+						printf("Speed:%03.02f[km/s]:%05.02f[m/s]", speed_km_h, speed_m_s);
+					}
+
+					// 前回時刻を更新
+					stLast = stTrue;
+				}
+			}else{
+				// 取得失敗
+				printf("ch[%02d]:failed      \n",0);
+			}
+			loop_cnt++;
+
+			sleep(0);
+		}
+	}
+	catch(...)
+	{
+		printf("calcSpeedPerOn() catch!! \n");
+		iRet = -1;
+	}
+	return iRet;
+}
+
+
 
 /*
-3.速度の検出原理を推測する。
-・ローラーの直径とギヤ比から、ローラー回転パルス信号の1間隔の距離を計算する。
-ローラーの直径＝29.4[mm]
-ローラーの外周＝29.4[mm]×3.1415926＝92.36282244[mm]
-ローラーと回転検出部のギヤ比＝6:1
-ローラー回転パルス信号の1間隔の距離＝92.36282244[mm]×6＝554.17693464[mm]＝0.55417693464[m]
+ローラー回転パルス信号の1間隔の距離は、0.55417693464[m]と推測。
 
-・車を走行させて、計測した速度と回転検出信号の周波数から、ローラー回転パルスの1間隔の距離を計算する。
-ミニ四駆カーを走行させたら、14km/hを表示。
-ローラーの回転信号の周波数はデジタルマルチメータの測定で約7Hz。
-計測速度14[km/h]＝14,000[m/h]＝14,000[m] / 3600[s]＝3.8888889[m/s]
-ローラー回転パルス信号の1間隔の距離＝3.8888889[m/s] / 7[pulse/s]＝0.5555556[m]
+distance[m] = 0.55417693464
+distance[m] = speed[m/s] / pulse[Hz]
 
-以上の結果から、
-ローラー回転パルス信号の1間隔の距離は、0.55417693464[m]と推測する。
+speed[m/s] = distance[m] * pulse[Hz]
+speed[km/h] = (distance[m] * pulse[Hz] ) / 1000[m]  * 3600[s]
 
-length[m] = speed[m/s] / pulse[Hz]
-
-speed[m/s] = length[m] * pulse[Hz]
-speed[km/h] = (length[m] * pulse[Hz] ) / 1000[m]  / 3600[s]
-length[m] = 0.55417693464
 
 # pulseの検出方法
 while()が1秒間立ったら、閾値以上のret_valueが何回きたかカウントする
