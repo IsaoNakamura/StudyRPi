e431a23cf440488b1c6ab077e33b69da1cdf551a
IsaoNakamura <gariox7@gmail.com>
2017-11-10 18:22:13 +0900

    Working.

--- Hubot/iotbot/my_exec/stillpi.sh
+++ Hubot/iotbot/my_exec/stillpi.sh
@@ -3,20 +3,20 @@
 # echo "arg_num : $#"
 
 dir=/home/pi/picam
+if [ -e $dir ]; then
+    echo "dir is exist."
+else
+    echo "dir is-not exist."
+    mkdir $dir
+fi
 
 if [ $# -eq 0 ]; then
-    if [ -e $dir ]; then
-        echo "dir is exist."
-    else
-        echo "dir is-not exist."
-        mkdir $dir
-    fi
     # echo "no arg"
     DATE=$(date +"%Y-%m-%d_%H%M")
     # echo "date    : $DATE"
     echo "raspistill -o /home/pi/picam/$DATE.jpg"
-    raspistill -o /home/pi/picam/$DATE.jpg
+    sudo raspistill -o /home/pi/picam/$DATE.jpg
 else
     echo "raspistill -o $@"
-    raspistill -o $@
+    sudo raspistill -o $@
 fi
