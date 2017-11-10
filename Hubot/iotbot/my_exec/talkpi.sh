112227b8abc4c87607ce250de9805948e2ff5ad0
IsaoNakamura <gariox7@gmail.com>
2017-11-10 17:31:20 +0900

    Working.

--- Hubot/iotbot/my_exec/talkpi.sh
+++ Hubot/iotbot/my_exec/talkpi.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
 cd /home/pi/aquestalkpi
-./AquesTalkPi -v f1 $@ | aplay -D plughw
+
+# AquesTalkPi's Options
+#  -v f1/f2  : Type
+#  -s 50-300 : Speed
+#  -g 0-100  : Volume
+#  -b        : Bouyomi
+#  -o a.wav  : Output
+./AquesTalkPi -s 60 -v f1 $@ | aplay -D plughw
 cd /home/pi
