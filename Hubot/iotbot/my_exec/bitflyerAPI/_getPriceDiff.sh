48db6ee06ca22d37bce895e723067b8443663bb5
IsaoNakamura <gariox7@gmail.com>
2017-12-12 14:56:37 +0900

    Working.

--- Hubot/iotbot/my_exec/bitflyerAPI/_getPriceDiff.sh
+++ Hubot/iotbot/my_exec/bitflyerAPI/_getPriceDiff.sh
@@ -3,8 +3,8 @@
 dir=/home/pi/GitHub/StudyRPi/Hubot/iotbot/my_exec/bitflyerAPI
 url=https://bitflyer.jp/api/echo/price
 
-echo dir=$dir
-echo url=$url
+echo dir=$dir >&1
+echo url=$url >&1
 
 $dir/getPriceDiff.pl $url $dir/DEST/result.json $@
 
