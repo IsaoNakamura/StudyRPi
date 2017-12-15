0ae8ea23ecdc375dfcd3fe00e94215150ea451fb
IsaoNakamura <gariox7@gmail.com>
2017-12-15 11:36:18 +0900

    Test.

--- Hubot/iotbot/my_exec/bitflyerAPI/_getPriceList.sh
+++ Hubot/iotbot/my_exec/bitflyerAPI/_getPriceList.sh
@@ -6,5 +6,5 @@ dest=$path/DEST/PriceList.json
 graph=$path/DEST/PriceList.png
 test=1
 
-sudo $path/getPriceList.pl $host $dest $graph $test $@
+# sudo $path/getPriceList.pl $host $dest $graph $test $@
 
