6c3517e8a9e5524c53a589f16b60f3dc00943d98
IsaoNakamura <gariox7@gmail.com>
2017-12-17 01:27:49 +0900

    Working.

--- Hubot/iotbot/my_exec/bitflyerAPI/_slackUpload.sh
+++ Hubot/iotbot/my_exec/bitflyerAPI/_slackUpload.sh
@@ -5,4 +5,4 @@ token=$2
 channel=$3
 filepath=$4
 
-curl --silent -F file=@$filepath -F channels=$channel -F token=$token $host
+curl -o /dev/null -w '%{http_code}\n' -s -F file=@$filepath -F channels=$channel -F token=$token $host
