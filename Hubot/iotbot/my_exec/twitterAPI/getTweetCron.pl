9089eb5691441c25897b2b8d85c222bb26ac0750
IsaoNakamura <gariox7@gmail.com>
2017-12-24 16:43:03 +0900

    Working.

--- Hubot/iotbot/my_exec/twitterAPI/getTweetCron.pl
+++ Hubot/iotbot/my_exec/twitterAPI/getTweetCron.pl
@@ -120,6 +120,20 @@ while(1){
                     my $text = $tweet_ref->{"text"};
                     # print $text . "\n";
                     $tweet_text = $text ."\n";
+                    
+                    # http抜き出し
+                    my $beg_pos = index($text, "http");
+                    if($beg_pos != -1){
+                        my $end_pos = index($text, "\s");
+                        if($end_pos==-1){
+                            $end_pos = index($text, "　");
+                            if($end_pos==-1){
+                                $end_pos = index($text, "\n");
+                            }
+                        }
+                        if($end_pos != -1){
+                        }
+                    }
                 }
 
                 # 添付ファイルURL取得
@@ -215,8 +229,18 @@ while(1){
                 my $post_text = "";
                 if($rt_quoted eq ""){
                     $post_text = $tweet_date . "```\n" . $tweet_link . $tweet_text . "```\n" . $quoted_text . $extended_text;
+                    my @array = ();
+                    getHttpStrArray(\@array, $tweet_text, 0);
+                    for(my $k=0;$k<@array;$k++){
+                        $post_text = $post_text . $array[$k] . "\n";
+                    }
                 }else{
                     $post_text = $rt_date . "```\n" . $tweet_link . $rt_text . "```\n" . $rt_quoted . $rt_extended;
+                    my @array = ();
+                    getHttpStrArray(\@array, $rt_text, 0);
+                    for(my $k=0;$k<@array;$k++){
+                        $post_text = $post_text . $array[$k] . "\n";
+                    }
                 }
                 
                 my $req = POST ($host,
@@ -360,4 +384,60 @@ sub convertTimeTZtoJST{
     $$jst_ref = UnixDate($date,"%Y/%m/%d %H:%M:%S");
 	
 	return (0);
+}
+
+sub getHttpStrArray{
+	my $array_ref	= shift; # OUT
+	my $text	    = shift; #  IN
+    my $pos         = shift; #  IN
+
+    # http抜き出し
+    my $beg_pos = index($text, "http",$pos);
+    #print "beg_pos=$beg_pos\n";
+    if($beg_pos != -1){
+        my $end_space = index($text, " ",$beg_pos+1);
+        #print "end_space=$end_space space.\n";
+
+        my $end_big = index($text, "　",$beg_pos+1);
+        #print "end_big=$end_big big space.\n";
+
+        my $end_ret = index($text, "\n",$beg_pos+1);
+        #print "end_ret=$end_ret return.\n";
+
+        my $end_pos = -1;
+        if($end_space!=-1){
+            $end_pos = $end_space - 1;
+        }
+        if($end_big!=-1){
+            if($end_pos==-1){
+                $end_pos = $end_big - 1;
+            }else{
+                if($end_pos>$end_big){
+                    $end_pos = $end_big - 1;
+                }
+            }
+        }
+        if($end_ret!=-1){
+            if($end_pos==-1){
+                $end_pos = $end_ret - 1;
+            }else{
+                if($end_pos>$end_ret){
+                    $end_pos = $end_ret - 1;
+                }
+            }
+        }
+        if($end_pos == -1){
+            $end_pos = length($text)-1;
+            #print "end_pos=$end_pos end of string.\n";
+        }
+        #print "!!! end_pos=$end_pos !!!\n";
+        if($end_pos != -1){
+            my $http_text = substr($text,$beg_pos,($end_pos-$beg_pos)+1);
+            push(@{$array_ref}, $http_text );
+            #print "push http_text=$http_text\n";
+            if($end_pos != length($text)){
+                getHttpStrArray($array_ref, $text, $end_pos+1);
+            }
+        }
+    }
 }
