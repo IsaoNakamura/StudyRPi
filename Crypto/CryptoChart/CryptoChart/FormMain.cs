e375e396a39f3115a4c9b8af5e56526cda0072ce
IsaoNakamura <gariox7@gmail.com>
2018-04-10 20:56:48 +0900

    Working.

--- Crypto/CryptoChart/CryptoChart/FormMain.cs
+++ Crypto/CryptoChart/CryptoChart/FormMain.cs
@@ -167,7 +167,10 @@ namespace CryptoChart
                 m_area.AxisY.Minimum = y_min - 1000;
                 m_area.AxisY.Maximum = y_max + 1000;
 
+                
                 this.chart1.Series.Add(m_series_ltp);
+
+                // Console.WriteLine("update Chart. y_min={0} y_max={1}", y_min, y_max);
             }
             catch (Exception ex)
             {
@@ -180,29 +183,31 @@ namespace CryptoChart
             return result;
         }
 
-        // 過去のキャンドルを取得
-        private async void getPastCandlestick()
+        // 過去のキャンドル情報群をバッファに適用
+        private int applyCandlestick(ref BitflyerOhlc ohlc)
         {
+            int result = 0;
             try
             {
-                // Cryptowatchから過去のデータを取得
-                BitflyerOhlc ohlc = await BitflyerOhlc.GetOhlcAfterAsync(m_productCodeCryptowatch, m_candleLength, 60);
                 if (ohlc == null)
                 {
                     Console.WriteLine("failed to GetOhlcAfterAsync()");
-                    return;
+                    result = -1;
+                    return result;
                 }
 
                 if (ohlc.result == null)
                 {
                     Console.WriteLine("ohlc's result is null");
-                    return;
+                    result = -1;
+                    return result;
                 }
 
                 if (ohlc.result.miniute == null)
                 {
                     Console.WriteLine("ohlc's miniute is null");
-                    return;
+                    result = -1;
+                    return result;
                 }
 
                 foreach (List<double> candleFactor in ohlc.result.miniute)
@@ -219,7 +224,9 @@ namespace CryptoChart
                     double closePrice = candleFactor[4];
                     double volume = candleFactor[5];
 
+                    // 2018/04/10 19:21:00
                     DateTime timestamp = DateTimeOffset.FromUnixTimeSeconds((long)closeTime).LocalDateTime;
+                    Console.WriteLine(timestamp);
 
                     Candlestick candle = m_candleBuf.addCandle(highPrice, lowPrice, openPrice, closePrice, timestamp.ToString());
                     if (candle == null)
@@ -233,42 +240,42 @@ namespace CryptoChart
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
+                result = -1;
             }
             finally
             {
             }
-            return;
+            return result;
         }
 
         private async void MainLoop()
         {
             try
             {
-                // 過去のキャンドルを取得
-                getPastCandlestick();
+
+                // Cryptowatchから過去のデータを取得
+                BitflyerOhlc ohlc = await BitflyerOhlc.GetOhlcAfterAsync(m_productCodeCryptowatch, m_candleLength, 60);
+                if (applyCandlestick(ref ohlc) != 0)
+                {
+                    Console.WriteLine("failed to applyCandlestick()");
+                    return;
+                }
+                // 過去データから一番新しい(最後)タイムスタンプを取得
+                List<double> candleLastFactor = ohlc.result.miniute.Last();
+                double lastCloseTime = candleLastFactor[0];
 
                 // CandleStick用
                 double high_price = 0.0;
                 double low_price = 0.0;
 
                 int pre_tick_id = 0;
-                DateTime prev = DateTime.Now;
+                DateTime prev_timestamp = DateTimeOffset.FromUnixTimeSeconds((long)lastCloseTime).LocalDateTime;
                 int cycle_cnt = 0;
 
                 Candlestick curCandle = null;
                 while (true)
                 {
-                    // 一定時間経ったか算出
-                    bool isMinit = false;
-                    
-                    DateTime now = DateTime.Now;
-                    TimeSpan span = now - prev;
-                    double elapsed_time = span.TotalSeconds;
-                    if (elapsed_time >= m_candleLength) {
-                        //Console.WriteLine(elapsed_time);
-                        prev = now;
-                        isMinit = true;
-                    }
+
 
                     // Tickerを取得
                     Ticker ticker = await Ticker.GetTickerAsync(m_productCodeBitflyer);
@@ -278,8 +285,35 @@ namespace CryptoChart
                     }
                     int tick_id = ticker.tick_id;
                     double cur_value = ticker.ltp;
-                    string timestamp = ticker.timestamp;
 
+                    DateTime dateTimeUtc = DateTime.Parse(ticker.timestamp);// 2018-04-10T10:34:16.677 UTCタイム
+                    DateTime cur_timestamp = System.TimeZoneInfo.ConvertTimeFromUtc(dateTimeUtc, System.TimeZoneInfo.Local);
+
+
+                    // 過去取得したキャンドルデータと離れ過ぎていないかチェック
+                    TimeSpan span = cur_timestamp - prev_timestamp;
+                    double elapsed_time = span.TotalSeconds;
+                    if (elapsed_time >= 61.0)
+                    {
+                        Console.WriteLine("Too Far!!. elapsed={0}, prev={1}, cur={2}", elapsed_time, prev_timestamp.ToString(), cur_timestamp.ToString());
+                        break;
+                    }
+
+                    // 過去取得したキャンドルの方が新しくないかチェック
+                    if (prev_timestamp.Minute > cur_timestamp.Minute)
+                    {
+                        Console.WriteLine("prev is new. prev={0}, cur={1}", prev_timestamp.ToString(), cur_timestamp.ToString());
+                        continue;
+                    }
+
+                    // キャンドルを閉じるべきか判断
+                    bool isClose = false;
+                    if (prev_timestamp.Minute < cur_timestamp.Minute)
+                    {
+                        Console.WriteLine("Need Close. prev={0}, cur={1}", prev_timestamp.ToString(), cur_timestamp.ToString());
+                        prev_timestamp = cur_timestamp;
+                        isClose = true;
+                    }
 
                     if (pre_tick_id == tick_id)
                     {
@@ -306,10 +340,11 @@ namespace CryptoChart
                         low_price = cur_value;
                     }
 
-                    if (isMinit == true || cycle_cnt == 0)
+                    if (isClose == true || cycle_cnt==0)
                     {
                         // 新たなキャンドルを追加
-                        curCandle = m_candleBuf.addCandle(high_price, low_price, (high_price + low_price) / 2.0, cur_value, timestamp);
+                        Console.WriteLine("add Candle. timestamp={0}", cur_timestamp.ToString());
+                        curCandle = m_candleBuf.addCandle(high_price, low_price, (high_price + low_price) / 2.0, cur_value, cur_timestamp.ToString());
                         if (curCandle == null)
                         {
                             Console.WriteLine("failed to addCandle.");
@@ -317,7 +352,7 @@ namespace CryptoChart
                         }
 
                         // 最高値・最低値リセット
-                        if (isMinit)
+                        if (isClose)
                         {
                             high_price = 0.0;
                             low_price = 0.0;
@@ -329,11 +364,13 @@ namespace CryptoChart
                         // 現在のキャンドルを更新
                         if (curCandle != null)
                         {
+                            
                             curCandle.high = high_price;
                             curCandle.low = low_price;
                             curCandle.open = (high_price + low_price) / 2.0;
                             curCandle.last = cur_value;
-                            curCandle.timestamp = timestamp;
+                            curCandle.timestamp = cur_timestamp.ToString();
+                            // Console.WriteLine("update Candle. timestamp={0}", cur_timestamp);
                         }
                     }
 
