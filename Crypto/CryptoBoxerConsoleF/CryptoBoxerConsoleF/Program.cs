432582e6bc4e5d646984bda7d5dcd86edda4a99d
IsaoNakamura <gariox7@gmail.com>
2018-04-13 18:50:43 +0900

    add Console App for .net framework

---
+++ Crypto/CryptoBoxerConsoleF/CryptoBoxerConsoleF/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CryptoBoxer;
+
+namespace CryptoBoxerConsoleF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boxer m_boxer = Boxer.createBoxer(null);
+            m_boxer.MainLoop();
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1);
+            }
+        }
+    }
+}
