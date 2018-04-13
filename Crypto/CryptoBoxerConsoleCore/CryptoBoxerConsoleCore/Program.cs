828f5adaa832edaca6c34046f11f8e9449086c32
IsaoNakamura <gariox7@gmail.com>
2018-04-13 19:03:57 +0900

    Add Console-APP for .NET CORE

---
+++ Crypto/CryptoBoxerConsoleCore/CryptoBoxerConsoleCore/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using CryptoBoxer;
+
+namespace CryptoBoxerConsoleCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boxer m_boxer = Boxer.createBoxer(null);
+            m_boxer.MainLoop();
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(0);
+            }
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
